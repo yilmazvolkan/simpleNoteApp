627b0a174c15f602c2ebb6b3c4161276557469f4
Volkan <volkanyilmaz@lyrebirdstudio.net>
2020-11-23 19:20:17 +0300

    Implemented add function with view states in fragment

--- app/src/main/java/com/yilmazvolkan/simplenoteapp/viewModels/NoteListViewModel.kt
+++ app/src/main/java/com/yilmazvolkan/simplenoteapp/viewModels/NoteListViewModel.kt
@@ -2,11 +2,17 @@ package com.yilmazvolkan.simplenoteapp.viewModels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.yilmazvolkan.simplenoteapp.view.NoteFragmentViewState
 import com.yilmazvolkan.simplenoteapp.view.NoteItemViewState
 
 class NoteListViewModel(app: Application) : AndroidViewModel(app) {
 
-    private val myEffectViewStates = arrayListOf<NoteItemViewState>(
+    private val noteScreenViewStateLiveData =
+        MutableLiveData<NoteFragmentViewState>().apply { value = NoteFragmentViewState(isAddClicked = false) }
+
+    private val myNotesList = arrayListOf<NoteItemViewState>(
         NoteItemViewState("1", "d", "", "date", false),
         NoteItemViewState("2", "d", "", "date", true),
         NoteItemViewState("3", "d", "", "date", false),
@@ -15,5 +21,17 @@ class NoteListViewModel(app: Application) : AndroidViewModel(app) {
     )
 
     fun getEffectSelectedViewStates(): ArrayList<NoteItemViewState> =
-        myEffectViewStates
+        myNotesList
+
+    fun addEffectSelectedViewState(noteItemViewState: NoteItemViewState){
+        myNotesList.add(noteItemViewState)
+    }
+
+    fun getNoteScreenViewStateLiveData(): LiveData<NoteFragmentViewState> =
+        noteScreenViewStateLiveData
+
+    fun notifyNoteScreenViewStateLiveData(isAddClicked: Boolean) {
+        noteScreenViewStateLiveData.value = NoteFragmentViewState(isAddClicked = isAddClicked)
+    }
+
 }
